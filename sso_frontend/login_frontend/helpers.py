963dc4e358a9d28fecd894e00601420e071e1a16
Olli Jarva <olli@jarva.fi>
2014-02-01 17:58:07 +0200

    BackURLValidator is per-SSO functionality.

--- sso_frontend/login_frontend/helpers.py
+++ sso_frontend/login_frontend/helpers.py
@@ -1,30 +1,5 @@
-from urlparse import urlparse
 import datetime
 import dateutil.parser
-
-class BackUrlValidator:
-    def __init__(self, back_url, valid_domains):
-        self.parsed_url = urlparse(back_url)
-        self.invalid = False 
-        self.hostname = self.parsed_url.hostname
-        if self.parsed_url.scheme is not "https":
-            self.invalid_scheme = True
-            self.invalid = True
-
-        if self.parsed_url.hostname:
-            for domain in valid_domains:
-                if self.parsed_url.hostname.endswith(domain):
-                    break
-            else:
-                self.server_not_allowed = True
-                self.invalid = True
-        else:
-            self.server_not_allowed = True
-            self.invalid = True
-            self.hostname = "-"
-
-        self.url = back_url
-
 
 def is_authenticated(request):
     if request.session.get("relogin_time"):
