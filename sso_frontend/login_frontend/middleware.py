776b8fe77525b8e26df862198a069fa87ad40f02
Olli Jarva <olli@jarva.fi>
2014-02-09 18:30:53 +0200

    Check session browser IDs properly

--- sso_frontend/login_frontend/middleware.py
+++ sso_frontend/login_frontend/middleware.py
@@ -21,6 +21,12 @@ def get_browser(request):
 class BrowserMiddleware(object):
     def process_request(self, request):
         request.browser = get_browser(request)
+        if (request.browser and not request.browser.valid_session_bid
+                    and not request.browser.save_browser):
+            # Browser is not saved, and it was restarted.
+            # Ensure everything is removed.
+            # Browser ID does not change.
+            request.browser.logout(request)
 
     def process_response(self, request, response):
         # Browser from process_request is not available here.
@@ -30,6 +36,9 @@ class BrowserMiddleware(object):
             return response
 
         if not browser.valid_session_bid:
+            # No valid session ID exists. Regen it first.
+            browser.bid_session = create_browser_uuid()
+            browser.save()
             cookies = browser.get_cookie()
             for cookie_name, cookie in cookies:
                response.set_cookie(cookie_name, **cookie)
