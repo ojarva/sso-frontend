29f01bb9ed217bb8d2fcd67fec0cea776091d1ee
Olli Jarva <olli@jarva.fi>
2014-02-16 18:49:18 +0200

    Moved user refreshing out from commands module.

--- sso_frontend/login_frontend/management/commands/refresh_users.py
+++ sso_frontend/login_frontend/management/commands/refresh_users.py
@@ -2,13 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User as DjangoUser
 from django.utils import timezone
 
+from login_frontend.utils import refresh_user
 from login_frontend.models import User
 import slumber
 import _slumber_auth
 import redis
 
-
-from login_frontend.config import Config
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -16,35 +16,14 @@ class Command(BaseCommand):
     help = 'Fetches all users'
 
     def handle(self, *args, **options):
-        config = Config()
-
-        api = slumber.API(config.get("fum-api-endpoint"), auth=_slumber_auth.TokenAuth(config.get("fum-api-token")))
+        api = slumber.API(settings.FUM_API_ENDPOINT, auth=_slumber_auth.TokenAuth(settings.FUM_ACCESS_TOKEN))
 
         c = 1
         while True:
             data = api.users.get(page=c)
             for user in data["results"]:
-                username = user.get("username")
-                first_name = user.get("first_name")
-                last_name = user.get("last_name")
-                email = user.get("email", "")
-                phone1 = user.get("phone1", "")
-                phone2 = user.get("phone2")
-                if username is None or email is None:
-                    continue
-                if first_name is None or last_name is None:
-                    self.stderr.write("Missing first or last name: %s" % username)
-                    continue
-
-                (user, created1) = DjangoUser.objects.get_or_create(username=username, defaults={"email": email, "is_staff": False, "is_active": True, "is_superuser": False, "last_login": timezone.now(), "date_joined": timezone.now()})
-                user.email = email
-                user.first_name = first_name
-                user.last_name = last_name
-                user.save()
-
-                (obj, created2) = User.objects.get_or_create(username=username)
-                changed = obj.refresh_strong(email, phone1, phone2, created=created2)
-                if changed or created1 or created2:
+                status = refresh_user(user)
+                if status:
                     self.stdout.write('Refreshed %s (%s, %s, %s)' % (username, email, phone1, phone2))
             if "next" not in data or data["next"] is None:
                 break
--- sso_frontend/login_frontend/utils.py
+++ sso_frontend/login_frontend/utils.py
@@ -1,14 +1,19 @@
+from django.conf import settings
+from django.conf import settings
+from django.contrib.auth.models import User as DjangoUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.conf import settings
-from models import Browser
+from django.utils import timezone
+from models import Browser, User
+import _slumber_auth
 import geoip2
 import geoip2.database
-import geoip2.models
 import geoip2.errors
-import urllib
+import geoip2.models
 import ipaddr
+import slumber
+import urllib
 
 geo = geoip2.database.Reader(settings.GEOIP_DB)
 IP_NETWORKS = settings.IP_NETWORKS
@@ -28,6 +33,35 @@ def is_private_net(ip_address):
             ip in network)):
             return description
     return False
+
+
+def get_and_refresh_user(username):
+    api = slumber.API(settings.FUM_API_ENDPOINT, auth=_slumber_auth.TokenAuth(settings.FUM_ACCESS_TOKEN))
+    refresh_user(api.users().get(username=username))
+
+def refresh_user(user):
+    username = user.get("username")
+    first_name = user.get("first_name")
+    last_name = user.get("last_name")
+    email = user.get("email", "")
+    phone1 = user.get("phone1", "")
+    phone2 = user.get("phone2")
+    if username is None or email is None:
+        return
+    if first_name is None or last_name is None:
+        return
+
+    (user, created1) = DjangoUser.objects.get_or_create(username=username, 
+            defaults={"email": email, "is_staff": False, "is_active": True, "is_superuser": False, "last_login": timezone.now(), "date_joined": timezone.now()})
+    user.email = email
+    user.first_name = first_name
+    user.last_name = last_name
+    user.save()
+
+    (obj, created2) = User.objects.get_or_create(username=username)
+    changed = obj.refresh_strong(email, phone1, phone2, created=created2)
+    if changed or created1 or created2:
+        return True
 
 def get_geoip_string(ip_address):
     private_net = is_private_net(ip_address)
