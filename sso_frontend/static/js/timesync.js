2b04c46d75795173047c1c009659a7bc39e9c65c
Olli Jarva <olli@jarva.fi>
2014-03-04 19:06:42 +0200

    Restructuring timesync client code

--- sso_frontend/static/js/timesync.js
+++ sso_frontend/static/js/timesync.js
@@ -2,24 +2,35 @@ function get_time() {
  return (new Date()).getTime();
 }
 
+function ping_roundtrip(random_id, tz_off, counter, data) {
+ counter = counter - 1;
+ var send_timestamp = get_time();
+ var results = false;
+ var url = "/timesync/"+random_id+"/"+tz_off+"/"+send_timestamp;
+ if (data) {
+  url += "/"+data.server_time;
+ }
+ if (counter < 1) {
+  url += "/results";
+  results = true;
+ }
+ $.ajax({
+   "url": url,
+   "error": function(jqxhr, textstatus, errorthrown) {
+    $("#timesync_report").html("An error occured: "+errorthrown);
+   }
+ }).done(function(data) {
+  if (results) {
+   $("#timesync_report").html(data.report);
+  } else {
+   ping_roundtrip(random_id, tz_off, counter, data);
+  }
+ });
+}
+
 $(document).ready(function() {
  var random_id = Math.floor(Math.random() * 100000000);
  var tz_off = date.getTimezoneOffset()
 
- send_timestamp = get_time()
- $.get("/timesync/"+random_id+"/"+tz_off+"/"+ send_timestamp, function(data) {
-  send_timestamp = get_time()
-  $.get("/timesync/"+random_id+"/"+tz_off+"/"+ send_timestamp + "/" + data.server_time, function(data) {
-   send_timestamp = get_time()
-   $.get("/timesync/"+random_id+"/"+tz_off+"/"+ send_timestamp + "/" + data.server_time, function(data) {
-    send_timestamp = get_time()
-    $.get("/timesync/"+random_id+"/"+tz_off+"/"+ send_timestamp + "/" + data.server_time, function(data) {
-     send_timestamp = get_time()
-     $.get("/timesync/"+random_id+"/"+tz_off+"/"+ send_timestamp + "/" + data.server_time+"/results", function(data) {
-      $("#timesync_report").html(data.report);
-     });
-    });
-   });
-  });
- });
+ ping_roundtrip(random_id, tz_off, 5);
 });
