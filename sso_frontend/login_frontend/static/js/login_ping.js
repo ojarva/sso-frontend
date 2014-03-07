8209ef0fd48a074d999d4f29ed6414eb1a9d0d81
Olli Jarva <olli@jarva.fi>
2014-03-07 03:41:06 +0200

    Increased ping interval & typo fix.

--- sso_frontend/login_frontend/static/js/login_ping.js
+++ sso_frontend/login_frontend/static/js/login_ping.js
@@ -1,5 +1,6 @@
 var login_ping_image = new Image();
+login_ping_image.src = "https://login.futurice.com/ping/external/img?location="+encodeURIComponent(window.location)+"&t="+(new Date()).getTime();
 function login_ping_update() {
  login_ping_image.src = "https://login.futurice.com/ping/external/img?location="+encodeURIComponent(window.location)+"&t="+(new Date()).getTime();
 }
-setInterval(ping_login_update, 120000);
+setInterval(login_ping_update, 300000);
