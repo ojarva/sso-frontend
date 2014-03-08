6b10ddaa132a6136cd4ac01aeb0251d0de9ca473
Olli Jarva <olli@jarva.fi>
2014-03-08 18:16:28 +0200

    Removed duplicate javascript from external ping javascript

--- sso_frontend/login_frontend/static/js/login_ping.js
+++ sso_frontend/login_frontend/static/js/login_ping.js
@@ -1,6 +1,6 @@
 var login_ping_image = new Image();
-login_ping_image.src = "https://login.futurice.com/ping/external/img?location="+encodeURIComponent(window.location)+"&t="+(new Date()).getTime();
 function login_ping_update() {
  login_ping_image.src = "https://login.futurice.com/ping/external/img?location="+encodeURIComponent(window.location)+"&t="+(new Date()).getTime();
 }
+login_ping_update();
 setInterval(login_ping_update, 300000);
